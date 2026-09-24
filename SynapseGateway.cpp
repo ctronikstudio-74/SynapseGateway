c28614d0384af06bd27e73dbf4c54bfe247fc41e
CTronikStudio <ctronikstudio@gmail.com>
2026-09-24 21:52:51 +0200

    first commit inizialize cmake project cross platform c++ 20

---
+++ SynapseGateway.cpp
@@ -0,0 +1,12 @@
+﻿// SynapseGateway.cpp : Defines the entry point for the application.
+//
+
+#include "SynapseGateway.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
